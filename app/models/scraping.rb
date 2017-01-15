8578e0f1586b25b6f92023944b59a8188dabb9f3
yukihiro <ilias_agamemnon@gmail.com>
2017-01-16 02:31:39 +0900

    任意の画面から不要なリンクを配列から回避

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -1,3 +1,5 @@
+
+
 class Scraping
   def self.shiro_urls
     links = []
@@ -5,20 +7,34 @@ class Scraping
     current_page = agent.get('https://ja.wikipedia.org/wiki/%E6%97%A5%E6%9C%AC%E3%81%AE%E5%9F%8E%E4%B8%80%E8%A6%A7')
     elements = current_page.search('ul li a')
     elements.each do |ele|
-      if ele.get_attribute('title') = "グスク"
-        break
-      end
+      break if ele.get_attribute('title') == "グスク"
+      next if ele.get_attribute('title') == "Wikipedia:曖昧さ回避"
+      next if ele.get_attribute('class') == "new"
       links << ele.get_attribute('href')
     end
 
-    links.each do |link|
-      compile_dt_shiro('https://ja.wikipedia.org' + link)
-    end
+    #links.each do |link|
+    #  compile_dt_shiro('https://ja.wikipedia.org' + link)
+    #end
   end
 
   def self.compile_dt_shiro(link)
+    
     agent = Mechanize.new
     page = agent.get(link)
+    #return unless page.search('#firstHeading').inner_text include?("作成中")
+
     puts page.search('#firstHeading').inner_text
+
+    #elements.page.search('span').each do |ele|
+    #  puts ele.get_attribute('title').inner_text if ele.get_attribute('title') == "この位置の地図やなどをリンクするページを表示します"
+    #end
+
+    #if page.search('a.external span').get_attribute('title') == "この位置の地図やなどをリンクするページを表示します"
+    #  puts page.search('a.external span').inner_text
+    #elsif page.search('span.latitude')
+    #  puts page.search('span.latitude').inner_text
+    #  puts page.search('span.longitude').inner_text
+    #end
   end
 end
